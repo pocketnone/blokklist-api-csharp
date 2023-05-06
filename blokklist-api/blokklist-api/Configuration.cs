e9e83a4a95fad2b47dea18952299a06b2b85fce4
NONE <43313338+pocketnone@users.noreply.github.com>
2023-05-06 15:55:40 +0200

    release

---
+++ blokklist-api/blokklist-api/Configuration.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+
+namespace blokklist_api
+{
+    public class Configuration
+    {
+        private string _authToken;
+        private Dictionary<string, string> _headers;
+        private string _userAgent;
+
+        public Configuration(string authToken, string userAgent)
+        {
+            _authToken = authToken;
+            _userAgent = userAgent;
+            _headers = new Dictionary<string, string>
+            {
+                { "Content-Type", "application/json" },
+                { "Authorization", $"Bearer {_authToken}" },
+                { "User-Agent", _userAgent }
+
+            };
+        }
+
+        public IReadOnlyDictionary<string, string> GetHeaders() => _headers;
+    }
+}
---
+++ blokklist-api/blokklist-api/blokklistapi.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace blokklist_api
+{
+    public class blokklistapi
+    {
+        private static readonly HttpClient client = new();
+
+        public async Task<dynamic> GetUserData(Configuration config, string urlToRequestUser, int _LimitReasons = 0)
+        {
+            var apiUrl = "https://api.none.mba/user/request";
+            var requestBody = new
+            {
+                UserID = urlToRequestUser,
+                Options = new { LimitReasons = _LimitReasons }
+            };
+
+            var jsonContent = JsonConvert.SerializeObject(requestBody);
+            var httpContent = new StringContent(jsonContent, Encoding.UTF8, "application/json");
+            httpContent.Headers.Clear();
+            foreach (var header in config.GetHeaders())
+            {
+                httpContent.Headers.Add(header.Key, header.Value);
+            }
+
+            var response = await client.PostAsync(apiUrl, httpContent);
+            response.EnsureSuccessStatusCode();
+
+            var responseData = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<dynamic>(responseData);
+        }
+    }
+}
